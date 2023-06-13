bc26e4f3775e96bba81dde0250b4f15bbe5fc241
Julien FRANCAIS <jfrancai@student.42.fr>
2023-06-14 01:43:04 +0200

    Part7 extracting BlogList into its own component

--- Part7/bloglist/frontend/src/App.js
+++ Part7/bloglist/frontend/src/App.js
@@ -1,16 +1,13 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Blog from './components/Blog'
-import { LoginForm, BlogForm } from './components/Form'
+import { LoginForm } from './components/Form'
+import BlogList from './components/BlogList'
 import Notification from './components/Notification'
-import Togglable from './components/Toggable'
 import { initBlogs } from './reducers/blogReducer'
-import { addUser, handleLogout, setUser, } from './reducers/userReducer'
-
+import { addUser, setUser } from './reducers/userReducer'
 
 const App = () => {
 	const dispatch = useDispatch()
-	const blogs = useSelector(state => state.blogs)
 	const user = useSelector(state => state.user)
 
 	useEffect(() => {
@@ -23,23 +20,7 @@ const App = () => {
 
 	const loginForm = () => < LoginForm setUser={ (user) => dispatch(setUser(user)) } />
 
-	const blogsList = () => {
-		const sortedBlogs = [...blogs].sort((a, b) => a.likes < b.likes)
-		return (
-			<>
-				<h2>blogs</h2>
-				<p>{user.username} logged in <button onClick={() => dispatch(handleLogout())} >log out</button></p>
-				<Togglable buttonLabelShow='new blog' buttonLabelHide='cancel'>
-					<BlogForm
-						blogs={blogs}
-					/>
-				</Togglable>
-				<br/>
-				<div>
-					{sortedBlogs.map(blog => <Blog key={blog.id} blog={blog} />) }
-				</div>
-			</>
-		)}
+	const blogsList = () => <BlogList />
 
 	return (
 		<div>
